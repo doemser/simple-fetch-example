051bf0225a456f5f8c84cc7c100a8c50ec04b46d
Dominik Hautau <dominikhautau@gmail.com>
2023-08-31 13:34:18 +0200

    mini example

---
+++ src/pages/api/first.js
@@ -0,0 +1,7 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  setTimeout(() => {
+    res.status(200).json({ name: "first thing finished" });
+  }, 3000);
+}
